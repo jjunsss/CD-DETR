ef66dac49c44c9989a7e3c5fd19e3f12226d8d19
junsukim <jjunsss@unist.ac.kr>
2023-07-31 15:19:08 +0900

    eval text

--- Custom_Dataset.py
+++ Custom_Dataset.py
@@ -107,9 +107,9 @@ def DivideTask_for_incre(args, Task_Counts: int, Total_Classes: int, DivisionOfN
                 )
         else:
             if args.LG:
-                Divided_Classes.append([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, ]) # DID + PZ
+                Divided_Classes.append([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,]) # DID + PZ
                 Divided_Classes.append([28, 32, 35, 41, 56]) # PZ 
-                Divided_Classes.append([24, 29, 30, 39, 40, 42]) # custom VE
+                # Divided_Classes.append([24, 29, 30, 39, 40, 42]) # custom VE
             else:                
                 Divided_Classes.append([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44])
                 Divided_Classes.append([46, 47, 48, 49, 50, 51, 52, 53, 54, 55])
